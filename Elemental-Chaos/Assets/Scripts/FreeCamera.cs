3e61b358d9dce4047fe374c8fcecdf8839b8c9ee
jesus2660 <jesusgp22@gmail.com>
2014-05-16 12:40:37 -0430

    Free Camera and Test Scene
    Added Free Camera Script and Test Scene + Materials and Textures

---
+++ Elemental-Chaos/Assets/Scripts/FreeCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreeCamera : MonoBehaviour {
+
+	float cameraSpeed = 1;
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		transform.Rotate (Vector3.up, Input.GetAxis("Mouse X")*3, Space.World);
+
+		transform.Rotate (Vector3.right, -Input.GetAxis("Mouse Y")*3, Space.Self);
+
+		if(Input.GetKey(KeyCode.A)){
+			transform.position = transform.position - transform.right*cameraSpeed*Time.fixedDeltaTime;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.position = transform.position + transform.right*cameraSpeed*Time.fixedDeltaTime;
+		}
+		if(Input.GetKey(KeyCode.W)){
+			transform.position = transform.position + transform.forward*cameraSpeed*Time.fixedDeltaTime;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.position = transform.position - transform.forward*cameraSpeed*Time.fixedDeltaTime;
+		}
+
+	}
+}
